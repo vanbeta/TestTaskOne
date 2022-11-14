b365614ba142496e6405160c477c0137eb015e4c
vanbeta <deimosbeta@gmail.com>
2022-11-14 16:50:55 +0300

    Initial Commit

---
+++ TestTaskOne/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestTaskOne
+//
+//  Created by Ivan Poderegin on 14.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
